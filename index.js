d7976950ed1c45a3db640a27228940804f818d10
pattysc <pattys@users.noreply.github.com>
2017-01-30 22:31:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const app = "I don't do much.";
+
+const Button = React.createClass({
+  render(){
+    return React.createElement('button', {}, 'Click me!')
+  }
+})
+
+const ShoppingList = React.createClass({
+  render(){
+    return React.createElement('ul', {}, [
+      React.createElement('li', {}, 'Bananas'),
+      React.createElement('li', {}, 'Vanilla ice cream'),
+      React.createElement('li', {}, 'Chocolate')
+    ])
+  }
+})
+
+class Button extends React.Component{
+  render(){
+    return React.createElement('button', {}, 'Click me!')
+  }
+}
